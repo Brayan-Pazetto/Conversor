149a26a64ebdb012d72f9034c6d576a3324d555e
Brayan-Pazetto <brayan.pazetto@gmail.com>
2024-04-10 09:19:50 -0300

    JS

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
 
-function convertValues(){
-   const InputCurrencyValue = document.querySelector(".input-currency").value
+function convertValues() {
+   const inputCurrencyValue = document.querySelector(".input-currency").value
+   const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
+   const currencyValueConverted = document.querySelector(".currency-value")
 
-const DolarToday = 5.2
+   const dolarToday = 5.2
+   const euroToday = 6.2
 
-const ConvertValue = InputCurrencyValue / DolarToday
+if(currencySelect.value == "dolar"){
+   currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US",{
+      style: "currency",
+      currency: "USD"
+   }).format(inputCurrencyValue / dolarToday)
 
-   console.log(ConvertValue)
+}
+if(currencySelect.value == "euro"){
+   currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE",{
+      style: "currency",
+      currency: "EUR"
+   }).format(inputCurrencyValue / euroToday)
+}
+
+   currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR",{
+      style: "currency",
+      currency: "BRL"
+   }).format(inputCurrencyValue)
+  
 }
 
 convertButton.addEventListener("click", convertValues)
