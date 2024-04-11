11fb83857758910b9e4a576eaafb73a55fa28ef0
Brayan-Pazetto <brayan.pazetto@gmail.com>
2024-04-11 08:11:51 -0300

    troquei td

--- script.js
+++ script.js
@@ -9,25 +9,42 @@ function convertValues() {
    const dolarToday = 5.2
    const euroToday = 6.2
 
-if(currencySelect.value == "dolar"){
-   currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US",{
-      style: "currency",
-      currency: "USD"
-   }).format(inputCurrencyValue / dolarToday)
+   if (currencySelect.value == "dolar") {
+      currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+         style: "currency",
+         currency: "USD"
+      }).format(inputCurrencyValue / dolarToday)
 
-}
-if(currencySelect.value == "euro"){
-   currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE",{
-      style: "currency",
-      currency: "EUR"
-   }).format(inputCurrencyValue / euroToday)
-}
+   }
+   if (currencySelect.value == "euro") {
+      currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+         style: "currency",
+         currency: "EUR"
+      }).format(inputCurrencyValue / euroToday)
+   }
 
-   currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR",{
+   currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
       style: "currency",
       currency: "BRL"
    }).format(inputCurrencyValue)
-  
+
 }
 
+function changeCurrency() {
+   const currencyName = document.getElementById("currency-name")
+   const currencyImage = document.querySelector(".currency-logo")
+
+   if (currencySelect.value == "dolar") {
+      currencyName.innerHTML = "dolar"
+      currencyImage.src = "./assets/estados-unidos (1) 1.png"
+   }
+
+   if (currencySelect.value == "euro") {
+      currencyName.innerHTML = "Euro"
+      currencyImage.src = "./assets/Design sem nome 3.png"
+   }
+
+   convertValues()
+}
+currencySelect.addEventListener("change", changeCurrency)
 convertButton.addEventListener("click", convertValues)
